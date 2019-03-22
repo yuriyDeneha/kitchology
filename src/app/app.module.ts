39e0d7c784c6da95fa58dc1b44d7791b8d9f7878
ViktoriiaBilaniuk <vika.bilaniuk@gmail.com>
2019-03-23 00:20:04 +0200

    project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {AppComponent} from './app.component';
-import {TransferHttpCacheModule} from '@nguniversal/common';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { TransferHttpCacheModule } from '@nguniversal/common';
+import { Section1Component } from './sections/section1/section1.component';
+import { Section2Component } from './sections/section2/section2.component';
+import { Section3Component } from './sections/section3/section3.component';
+import { Section4Component } from './sections/section4/section4.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
+    Section1Component,
+    Section2Component,
+    Section3Component,
+    Section4Component,
   ],
   imports: [
     BrowserModule,
