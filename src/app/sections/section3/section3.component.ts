5897cd5595dea6318f752a3dbc0e60d2906b49d8
ViktoriiaBilaniuk <vika.bilaniuk@gmail.com>
2019-03-23 00:40:44 +0200

    na Yura

---
+++ src/app/sections/section3/section3.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'kit-section3',
+  templateUrl: './section3.component.html',
+  styleUrls: ['./section3.component.scss']
+})
+export class Section3Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
